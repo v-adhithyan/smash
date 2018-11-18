a5fe08b2f14e4b2835a0644b642e6f6ff4d32650
v-adhithyan <tenmiles@tenmiless-MacBook-Air.local>
2018-11-18 17:34:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import renderHTML from 'react-render-html';
+
+//const API = 'https://poydev.pythonanywhere.com/api/v1/agenda/';
+const API = "http://localhost:8000/api/v1/agenda/"
+const rmj = require('render-markdown-js')
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      agendas: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API, {
+      crossDomain: true,
+      method: 'GET',
+    })
+      .then(response => {
+        console.log(response)
+        return response.json();
+      })
+      .then(data => this.setState({ agendas: data }));
+  }
+
   render() {
+    //const agenda  = this.state.agenda;
+    let agenda_raw = "";
+    console.log(this.state.agendas);
+    for(var i=0; i<this.state.agendas.length; i++) {
+      agenda_raw += this.state.agendas[i].agenda_text + "\n";
+    }
+    const agenda_html = rmj(agenda_raw)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='app'>
+        {renderHTML(agenda_html)}
       </div>
-    );
+    )
   }
 }
-
 export default App;
