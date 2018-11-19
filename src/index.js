9eba21509feb35e6dc27aa46a5bbc4b53a5d26d9
v-adhithyan <aavispeaks@gmail.com>
2018-11-19 20:01:15 +0530

    add agenda initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderHTML from 'react-render-html';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SideBar extends React.Component {
+    render() {
+        return (
+            <div className="sidebar" />
+        )
+    }
+}
+
+const API = "http://localhost:8000/api/v1/agenda/"
+const rmj = require('render-markdown-js')
+
+class App extends React.Component {
+    /*constructor(props) {
+        super(props);
+
+        this.state = {
+            agendas: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(API, {
+            crossDomain: true,
+            method: 'GET',
+        })
+            .then(response => {
+                console.log(response)
+                return response.json();
+            })
+            .then(data => this.setState({ agendas: data }));
+    }*/
+    renderSidebar() {
+        return <SideBar/>;
+    }
+
+    render() {
+        /*let agenda_raw = "";
+        console.log(this.state.agendas);
+        for (var i = 0; i < this.state.agendas.length; i++) {
+            agenda_raw += this.state.agendas[i].agenda_text + "\n";
+        }*/
+        let agenda_raw = "# Agenda for 19 Nov 2018\n - Complete React tutorial\n - Adhoc tasks\n - Read auth package\n"
+        const agenda_html = rmj(agenda_raw)
+        return (
+            <div className="sidebar">
+            <h1 className="header">Agenda</h1>
+                <div>
+                    {renderHTML(agenda_html)}
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
